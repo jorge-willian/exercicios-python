333991773288fce94f117cf78affaf6555769925
Jorge Willian de Oliveira <91788583+jorge-willian@users.noreply.github.com>
2022-01-02 15:43:12 -0300

    Update ex032.py

--- ex032.py
+++ ex032.py
@@ -1,9 +1,13 @@
+from datetime import date
+
 print('=-'*25)
 print(f'{str("ANO BISSEXTO"):^50}')
 print('=-'*25)
 
-ano = int(input('Digite o ano para verificar se ele é bissexto: '))
-if (ano % 4 == 0):
+ano = int(input('Digite o ano para verificar se ele é bissexto (0 para ano atual): '))
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print(f'O ano {ano} é bissexto.')
 else:
     print(f'O ano {ano} não é bissexto.')

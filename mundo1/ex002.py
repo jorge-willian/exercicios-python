2879a7ff4d4f3c3f4d40ab70df25b7bac92224fc
Jorge Willian de Oliveira <91788583+jorge-willian@users.noreply.github.com>
2022-02-11 15:42:22 -0300

    Update ex002.py

--- mundo1/ex002.py
+++ mundo1/ex002.py
@@ -1,8 +1,11 @@
+#Resolução 1
 nome = input('Qual seu nome? ')
 print(f'É um prazer te conhecer {nome}!')
 
+# Resolução 2
 name = input('Qual seu nome? ')
 print('É um prazer te conhecer', name,'!')
 
-n = input('Qual sei nome? ')
+#Resolução 3
+n = input('Qual seu nome? ')
 print('É um prazer te conhecer {}!'.format(n))

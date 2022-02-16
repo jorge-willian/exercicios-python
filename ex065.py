b74ed23471ff34bcbe1f8d7e5bb4d457bcbc71e0
Jorge Willian de Oliveira <91788583+jw-oliveira@users.noreply.github.com>
2022-02-16 16:50:59 -0300

    Add files via upload

---
+++ ex065.py
@@ -0,0 +1,16 @@
+print('~'*60)
+print(f'{"DESAFIO 065 - LER NÚMEROS, MOSTRAR MÉDIA, MAIOR E MENOR "}')
+print('~'*60)
+
+opcao = 'S'
+soma = cont = 0
+lista_num = []
+
+while opcao == 'S':
+    num = int(input('Digite um número: '))
+    opcao = str(input('Quer continuar? [S/N] ')).upper().strip()
+    soma += num
+    cont += 1
+    lista_num.append(num)
+print(f'Você digiou {cont} números e a média é de {soma / cont:.2f}.')
+print(f'O maior número digitado foi {max(lista_num)} e o menor número digitado foi {min(lista_num)}.')

4eb357631b93ac68d2d53c2a26b0995b1c5f6aad
Jorge Willian de Oliveira <91788583+jw-oliveira@users.noreply.github.com>
2022-04-08 09:38:24 -0300

    Update 073 - Tuplas com Times de Futebol.py

--- mundo3/073 - Tuplas com Times de Futebol.py
+++ mundo3/073 - Tuplas com Times de Futebol.py
@@ -1,13 +1,17 @@
-print(f'{" CAMPEONATO BRASILEIRO FEMININO ":=^50}')
+print(f'''{'='*50}
+{"EX073 - TUPLAS COM TIMES DE FUTEBOL":^50}
+{'='*50}''')
 
-times = ('Internacional','Ferroviária','Flamengo','São Paulo','Corinthians','Palmeiras','Santos','Real Brasilia','São José SP','Grêmio','Cruzeiro','Atlético MG','Avaí','Bragantino','Cresspom','Esmac')
+times = ('Corinthians','Palmeiras','Santos','Grêmio','Cruzeiro','Flamengo','Vasco',
+'Chapecoense','Atlético','Botafogo','Atlético - PR','Bahia','São Paulo','Fluminense',
+'Sport Recife','EC Vitória','Coritiba','Avaí','Ponte Preta','Atlético - GO')
 
-print('=-'*50)
+print('=-'*25)
 print(f'Os 5 primeiros colocados são {times[0:5]}')
-print('=-'*50)
-print(f'Os 4 últimos colocados são {times[-4::]}')
-print('=-'*50)
+print('=-'*25)
+print(f'Os 4 últimos colocados são {times[-4:]}')
+print('=-'*25)
 print(f'Os times em ordem alfabética são {sorted(times)}')
-print('=-'*50)
-print(f'O Grêmio está na {times.index("Grêmio") + 1}ª posição')
-print('=-'*50)
+print('=-'*25)
+print(f'O Chapecoense está na {times.index("Chapecoense") + 1}ª posição')
+print('=-'*25)
